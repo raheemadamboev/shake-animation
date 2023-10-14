5e67b9908dab548c948516381fd72110f3ab0b4e
raheemadamboev <vividricemaryandmax@gmail.com>
2023-10-14 12:39:19 +0500

    Added colors;

--- app/src/main/java/xyz/teamgravity/shake_animation/ui/theme/Color.kt
+++ app/src/main/java/xyz/teamgravity/shake_animation/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Red = Color(0xFFDD5D5D)
+val Green = Color(0xFF79DD5D)
+val White = Color(0xFFF7F7F7)
